895d6d7cf8b755f97a49d1466cc7d067f04ddf7d
SleepSupreme <linchenguo0622@gmail.com>
2021-08-07 23:28:14 +0800

    add 3d-nas

--- project/3d-nas/page.js
+++ project/3d-nas/page.js
@@ -1,8 +1,6 @@
-var games;
-var edges;
 var myGraph;
 var myData;
-var highlightLinks = [];
+var myControl;
 
 init();
 
@@ -12,335 +10,395 @@ var z = document.getElementById("left-float");
 x.style.display = "none";
 y.style.display = "none";
 z.style.display = "none";
-var colorList = [
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24)),
-    Math.round(Math.random() * Math.pow(2, 24))
-]
 
 
 function init() {
-    let select = document.getElementById("gselect");
-    select.addEventListener("change", onSelectChanged);
-    
-    d3.json("data/all_settings.json").then(function (data) {
+    let select_graph = document.getElementById("gselect");
+    select_graph.addEventListener("change", onGraphChanged);
+
+    let select_control = document.getElementById("cselect");
+    select_control.addEventListener("change", onControlChanged);
+
+    d3.json("data/all_settings_1200.json").then(function (data) {
         myData = data;
         loadAllSettings();
+        setTimeout(frozenGraph, 30000);
     });
 }
 
-function refreshHTML(node) {
-    document.getElementById("name").innerHTML = node.id;
-}
-
 function leftClick(node) {
-    refreshHTML(node);
-}
+    frozenGraph();
+    lookNode(node);
 
-function rightClick(node) {
-    refreshHTML(node);
-    var counter = 0;
-    highlightLinks = [];
-    let neighbors = [];
-    var imgs = "";
-    for (link of gData.links) {
+    let rows = "";
+    for (link of myData.links) {
         if (link.source.id == node.id) {
-            highlightLinks.push(link);
-            counter++;
-            neighbors.push(link.target);
-        }
-        if (link.target.id == node.id) {
-            highlightLinks.push(link);
-            counter++;
-            neighbors.push(link.source);
-            // console.log(link);
+            rows += tbodyFormat(link);
         }
     }
-    // document.getElementById("talltime").innerHTML = counter;
-    for (neigh of neighbors) {
-        imgs += posterFormat(neigh.id);
-    }
-    document.getElementById("neighbors").innerHTML = imgs;
-    // console.log(highlightLinks);
-    updateGeometries();
+
+    document.getElementById("name").innerHTML = node.id;
+    document.getElementById("mrows").innerHTML = rows;
+
+    document.getElementById("input_channels").innerHTML = node.input_channels;
+    document.getElementById("network_setting").innerHTML = node.network_setting;
+    document.getElementById("last_channels").innerHTML = node.last_channels;
+    document.getElementById("flops").innerHTML = node.flops;
+    document.getElementById("params").innerHTML = node.params;
+    document.getElementById("top1").innerHTML = node.top1;
+    document.getElementById("top5").innerHTML = node.top5;
+    document.getElementById("miou").innerHTML = node.miou;
+    document.getElementById("macc").innerHTML = node.macc;
+    document.getElementById("aacc").innerHTML = node.aacc;
+    document.getElementById("moderate").innerHTML = node.moderate;
 }
 
-function loadAllSettings() {   
+function loadAllSettings() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadCls501000() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadCls50100() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadCls101000() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadCls10c() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadSeg() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadSeg4x() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function load3DDet() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadVideo() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
 function loadVideop() {
     const Graph = ForceGraph3D({
-        controlType: 'trackball'
+        controlType: myControl,
+        rendererConfig: {
+            powerPreference: 'low-power'
+        }
     }) // trackball, orbit or fly
     (document.getElementById('3d-graph'))
     .graphData(myData)
+        .cooldownTime(30000)
+        .nodeVal(node => (node.metric + 0.1) * 8)
         .nodeLabel(node => node.id)
         .nodeAutoColorBy(node => node.type)
-        .nodeVal(node => (node.metric + 0.5) * 4)
         .linkWidth(2)
         .linkOpacity(0.08)
         .linkAutoColorBy(link => link.type)
         .onNodeClick(node => {
             leftClick(node);
         })
-        .onNodeRightClick(node => {
-            rightClick(node);
-        });
+
+    Graph.d3Force('link').distance(link => link.length ** 2);
 
     myGraph = Graph;
 }
 
-function onSelectChanged() {
-    var e = document.getElementById("gselect");
-    var value = e.options[e.selectedIndex].value;
-    var text = e.options[e.selectedIndex].text;
+function onGraphChanged() {
+    let e = document.getElementById("gselect");
+    let value = e.options[e.selectedIndex].value;
+    let text = e.options[e.selectedIndex].text;
     document.getElementById("gtype").innerHTML = text;
     if (value == "1") {
-        loadAllSettings();
+        d3.json("data/all_settings_1200.json").then(function (data) {
+            myData = data;
+            loadAllSettings();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "2") {
-        loadCls501000();
+        d3.json("data/cls-50-1000.json").then(function (data) {
+            myData = data;
+            loadCls501000();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "3") {
-        loadCls50100();
+        d3.json("data/cls-50-100.json").then(function (data) {
+            myData = data;
+            loadCls50100();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "4") {
-        loadCls101000();
+        d3.json("data/cls-10-1000.json").then(function (data) {
+            myData = data;
+            loadCls101000();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "5") {
-        loadCls10c();
+        d3.json("data/cls-10c.json").then(function (data) {
+            myData = data;
+            loadCls10c();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "6") {
-        loadSeg();
+        d3.json("data/seg.json").then(function (data) {
+            myData = data;
+            loadSeg();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "7") {
-        loadSeg4x();
+        d3.json("data/seg-4x.json").then(function (data) {
+            myData = data;
+            loadSeg4x();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "8") {
-        load3DDet();
+        d3.json("data/3ddet.json").then(function (data) {
+            myData = data;
+            load3DDet();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "9") {
-        loadVideo();
+        d3.json("data/video.json").then(function (data) {
+            myData = data;
+            loadVideo();
+            setTimeout(frozenGraph, 30000);
+        });
     } else if (value == "10") {
-        loadVideop();
+        d3.json("data/video-p.json").then(function (data) {
+            myData = data;
+            loadVideop();
+            setTimeout(frozenGraph, 30000);
+        });
     }
 }
 
-function updateGeometries() {
-    sg.nodeRelSize(4); // trigger update of 3d objects in scene
+function onControlChanged() {
+    let e = document.getElementById("cselect");
+    let value = e.options[e.selectedIndex].value;
+    if (value == "1") {
+        myControl = "trackball";
+    } else if (value == "2") {
+        myControl = "fly";
+    }
+
+    onGraphChanged();
 }
 
-function posterFormat(id) {
-    return "<a id=\"link\" href=\"https://steamdb.info/app/" + id + "/info/\" target=\"_blank\"><img class=\"info\" src=\"img/" + id + ".jpg\" style=\"width:100%; \"></a>";
-}
-
-function tbodyFormat(node) {
-    return "<tr class=\"row100 body\"><td class=\"cell100 column1\">" +
-        node.id + "</td><td class=\"cell100 column2\">" +
-        node.Name + "</td><td class=\"cell100 column3\">" +
-        node.price + "</td><td class=\"cell100 column4\">" +
-        node.company + "</td><td class=\"cell100 column5\">" +
-        node.date + "</td><td class=\"cell100 column6\">" +
-        node.Rating + "</td></tr>"
+function tbodyFormat(link) {
+    return "<tr class=\"row100 body\"><td class=\"column1\" style=\"cursor:pointer;color:deepskyblue\" onclick=\"findNode(this)\">" +
+        link.target.id + "</td><td class=\"column2\">" +
+        link.target.flops + "</td><td class=\"column2\">" +
+        link.target.params + "</td><td class=\"column2\">" +
+        link.target.top1 + "</td><td class=\"column2\">" +
+        link.target.top5 + "</td><td class=\"column2\">" +
+        link.target.miou + "</td><td class=\"column2\">" +
+        link.target.macc + "</td><td class=\"column2\">" +
+        link.target.aacc + "</td><td class=\"column2\">" +
+        link.target.moderate + "</td></tr>"
 }
 
 function openNavRight() {
-    var x = document.getElementById("right-float");
-    var y = document.getElementById("bottom-float");
+    let x = document.getElementById("right-float");
+    let y = document.getElementById("bottom-float");
     if (x.style.display === "none") {
         x.style.display = "block";
         y.style.display = "block";
@@ -351,7 +409,7 @@ function openNavRight() {
 }
 
 function openNavLeft() {
-    var x = document.getElementById("left-float");
+    let x = document.getElementById("left-float");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
@@ -360,21 +418,11 @@ function openNavLeft() {
 }
 
 function searchNode() {
-    var id = document.getElementById("searchbar").value;
-    var found = false;
+    let id = document.getElementById("searchbar").value;
+    let found = false;
     for (node of myData.nodes) {
         if (node.id == id) {
-            const distance = 40;
-            const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
-
-            myGraph.cameraPosition({
-                    x: node.x * distRatio,
-                    y: node.y * distRatio,
-                    z: node.z * distRatio
-                }, // new position
-                node, // lookAt ({ x, y, z })
-                3000 // ms transition duration
-            );
+            leftClick(node);
             found = true;
             break;
         }
@@ -382,4 +430,37 @@ function searchNode() {
     if (!found) {
         alert("Node not found!");
     }
+}
+
+function findNode(td) {
+    let id = td.innerText;
+    let found = false;
+    for (node of myData.nodes) {
+        if (node.id == id) {
+            leftClick(node);
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        alert("Node not found!");
+    }
+}
+
+function lookNode(node) {
+    const distance = 60;
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+
+    myGraph.cameraPosition({
+            x: node.x * distRatio,
+            y: node.y * distRatio,
+            z: node.z * distRatio
+        }, // new position
+        node, // lookAt ({ x, y, z })
+        3000 // ms transition duration
+    );
+}
+
+function frozenGraph() {
+    myGraph.cooldownTicks(0);
 }
